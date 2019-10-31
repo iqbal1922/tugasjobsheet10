ae2cc7c345d3e752557c3135df5fb8debd10698c
iqbal1922 <57090602+iqbal1922@users.noreply.github.com>
2019-10-31 11:14:25 +0700

    Add files via upload

---
+++ tugasjb10/src/tugasjb10/Destroyable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugasjb10;
+
+/**
+ *
+ * @author IQBAL
+ */
+public interface Destroyable {
+    void destroyed();
+}
